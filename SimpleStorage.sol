996b79afa3b45afbb9a12997f4b9da1a7205cfe9
Soos3D <99700157+soos3d@users.noreply.github.com>
2022-09-21 11:35:33 -0300

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+
+    uint favoriteNumber;
+
+    function saveNumber(uint _Number) public {
+        favoriteNumber = _Number;
+    }
+
+
+    function deleteNumber() public {
+        favoriteNumber = 0;
+    }
+
+
+    function getNumber() public view returns(uint) {
+        return favoriteNumber;
+    }
+}
